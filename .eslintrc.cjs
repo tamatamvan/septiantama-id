ef3f362ba92645dc6d7a2c4b398981ec6c3ae33d
Septian A Tama <sfirst.tama@gmail.com>
2023-07-09 09:23:49 +0700

    setup linter and prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,20 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'next/core-web-vitals',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  ignorePatterns: [
+    'node_modules',
+    '.next',
+    'out',
+    '**/*.cjs',
+    'next.config.js',
+  ],
+};
---
+++ .prettierrc.cjs
@@ -0,0 +1,5 @@
+/** @type {import("prettier").Options} */
+module.exports = {
+  semi: true,
+  singleQuote: true,
+};
