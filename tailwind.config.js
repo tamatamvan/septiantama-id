0fb6491bd14175d072b16b2239e48b2e9dcd1c74
Septian A Tama <sfirst.tama@gmail.com>
2023-07-14 09:30:36 +0700

    add custom css from hero section radial background

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'gradient-radial':
+          'radial-gradient(50% 50% at 50% 50%, #ffffff 0%, rgb(0, 0, 0) 100%);',
+      },
+    },
   },
   plugins: [],
 };
