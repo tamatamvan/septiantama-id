ba2579a344f6b63d704fee5a5fd5f034f3827ad2
Septian A Tama <sfirst.tama@gmail.com>
2023-07-14 15:26:52 +0700

    add resumeUrl to next.config.js as public env

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    resumeUrl:
+      'https://drive.google.com/file/d/1W9KrTMn3f46woD-SGpl49Hj5GrXthOJh/view?usp=sharing',
+  },
+};
 
 module.exports = nextConfig;
