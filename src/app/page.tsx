9863541b02ff159355dadfc5a5fc89a1238f89d9
Septian A Tama <sfirst.tama@gmail.com>
2023-07-18 17:10:57 +0700

    add title and description metadata, move Footer to generic components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+import { Metadata } from 'next';
+
 import { AboutMe } from './(home)/components/AboutMe';
 import { ContactMe } from './(home)/components/ContactMe/ContactMe';
-import { Footer } from './(home)/components/Footer';
 import { HeaderNav } from './(home)/components/HeaderNav';
+import { Footer } from '~/components/Footer';
+
+export const metadata: Metadata = {
+  title: "Septian Tama's Personal Website",
+  description:
+    "Hi I'm Tama, an experienced fullstack software engineer with main expertise in front-end web development.",
+};
 
 export default function Home() {
   return (
