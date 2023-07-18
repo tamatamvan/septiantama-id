f680566f6a73dc5ea5b5036e072cd953ee169193
Septian A Tama <sfirst.tama@gmail.com>
2023-07-18 10:04:58 +0700

    kurokocchi

--- src/app/(home)/components/AboutMe/AboutMe.tsx
+++ src/app/(home)/components/AboutMe/AboutMe.tsx
@@ -1,6 +1,7 @@
 import { Heading } from '~/components/Heading/Heading';
 import { Section } from '~/components/Section';
 import { Paragraph } from '~/components/Paragraph';
+import Image from 'next/image';
 
 export function AboutMe() {
   return (
@@ -8,19 +9,31 @@ export function AboutMe() {
       <Heading as="h2" className="mb-8 text-3xl underline">
         A lil bit about me
       </Heading>
-      <Paragraph className="max-w-prose">
-        I'm an experienced fullstack software engineer with main expertise in
-        front-end web development. <br /> A lifelong learner that loves
-        experimenting with new things, sharing knowledge with others, and help
-        to empower people in reaching their goals and potential.
-      </Paragraph>
-      <Paragraph>
-        I enjoy coding 💻, cooking 🍳, poetry✍🏼, and bonding with my cat 🐈‍⬛.
-      </Paragraph>
-      <Paragraph>
-        Sometimes I play FIFA & Football Manager ⚽️🎮 I also watch NBA 🏀
-        <br />I love the tactical and analytical side of the sports. 📊
-      </Paragraph>
+      <div className="flex flex-wrap">
+        <div>
+          <Paragraph className="max-w-prose">
+            I'm an experienced fullstack software engineer with main expertise
+            in front-end web development. <br /> A lifelong learner that loves
+            experimenting with new things, sharing knowledge with others, and
+            help to empower people in reaching their goals and potential.
+          </Paragraph>
+          <Paragraph>
+            I enjoy coding 💻, cooking 🍳, poetry✍🏼, and bonding with my cat 🐈‍⬛.
+          </Paragraph>
+          <Paragraph>
+            Sometimes I play FIFA & Football Manager ⚽️🎮 I also watch NBA 🏀
+            <br />I love the tactical and analytical side of the sports. 📊
+          </Paragraph>
+        </div>
+        <Image
+          src="/kurokocchi.png"
+          className="mx-auto my-8"
+          alt="Cat Illustration"
+          width={150}
+          height={218}
+          objectFit="contain"
+        />
+      </div>
     </Section>
   );
 }
