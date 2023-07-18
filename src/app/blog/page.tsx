c7d6d630988ba6806d68458182d7cd2e8fc51c5b
Septian A Tama <sfirst.tama@gmail.com>
2023-07-18 11:11:29 +0700

    create coming soon (for blog) and 404 page

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,31 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import { Heading } from '~/components/Heading/Heading';
+import { Paragraph } from '~/components/Paragraph';
+
+export default function BlogPage() {
+  return (
+    <div className="flex h-screen w-full flex-col items-center justify-center bg-black px-8 text-white">
+      <Heading as="h1" className="text-4xl">
+        Coming soon...
+      </Heading>
+      <Image
+        src="/kurokocchi_playing_yarn.svg"
+        alt="Kurokocchi plays"
+        width={320}
+        height={320}
+        objectFit="contain"
+      />
+      <Paragraph className="my-8 max-w-prose text-center">
+        Thank you for your interest, the blog is still under construction.
+        <br /> Until then, how about we play with Kurokocchi?
+      </Paragraph>
+      <Link
+        href="/"
+        className="block border-b-0 font-bold text-white transition-all ease-linear hover:-translate-y-1 hover:scale-110 hover:border-b-2 hover:border-b-white"
+      >
+        Back to homepage
+      </Link>
+    </div>
+  );
+}
---
+++ src/app/not-found.tsx
@@ -0,0 +1,34 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import { Heading } from '~/components/Heading/Heading';
+import { Paragraph } from '~/components/Paragraph';
+
+export default function BlogPage() {
+  return (
+    <div className="flex h-screen w-full flex-col items-center justify-center bg-black px-8 text-white">
+      <Heading as="h1" className="mb-4 text-4xl sm:text-5xl md:text-6xl">
+        404
+      </Heading>
+      <Heading as="h2" className="text-center text-2xl sm:text-3xl md:text-4xl">
+        Ummm... Seems that you're lost
+      </Heading>
+      <Image
+        src="/404_getting_lost.svg"
+        alt="404 Not Found"
+        width={320}
+        height={480}
+        objectFit="contain"
+      />
+      <Paragraph className="my-8 max-w-prose text-center">
+        The page you're looking for doesn't exist. Maybe check the URL again? Or
+        you can also...
+      </Paragraph>
+      <Link
+        href="/"
+        className="block border-b-0 font-bold text-white transition-all ease-linear hover:-translate-y-1 hover:scale-110 hover:border-b-2 hover:border-b-white"
+      >
+        Back to homepage
+      </Link>
+    </div>
+  );
+}
